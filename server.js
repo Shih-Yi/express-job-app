2f8c7304be3b184e88032d92aaf017c0634d453c
Shih-Yi <brvast@gmail.com>
2023-02-21 10:33:06 +1300

    add Error Middleware

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandleMiddleare from './middleware/error-handler.js';
 
 app.get('/', (req, res) => {
+  throw new Error('Error!')
   res.send('Welcome!');
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandleMiddleare);
 
 const port = process.env.PORT || 3001;
 
