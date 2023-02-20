ff8e933b5b2788da96c6fd30bcdf9cc1aa8824f5
Shih-Yi <brvast@gmail.com>
2023-02-21 12:15:18 +1300

    add authController and authRoutes Structure

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ dotenv.config();
 
 import connectDB from './db/connect.js';
 
+// routers
+import authRouter from './routes/authRoutes.js';
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandleMiddleare from './middleware/error-handler.js';
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandleMiddleare);
