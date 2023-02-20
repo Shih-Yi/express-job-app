02f9b4e8a8380732c49908f4edc8d8244f2db9bd
Shih-Yi <brvast@gmail.com>
2023-02-21 10:18:54 +1300

    add express and basic server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('server running...')
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
