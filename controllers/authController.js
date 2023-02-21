f65593e7d05839f545a7f24a79cff0cad92b2d3d
Shih-Yi <brvast@gmail.com>
2023-02-22 10:11:22 +1300

    add Custom Errors and BadRequestError

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,26 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message)
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.BAD_REQUEST
+  }
+}
+
 const register = async (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
+
   console.log(req.body)
   const user = await User.create(req.body)
   res.status(StatusCodes.CREATED).json({ user })
