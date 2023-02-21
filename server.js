9d0beb0de954484ee77b4b767a714049c3be6d38
Shih-Yi <brvast@gmail.com>
2023-02-21 18:20:02 +1300

    install express-async-errors package to handle error to next

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-import express from 'express';
-const app = express();
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express'
+const app = express()
+import 'express-async-errors'
+import dotenv from 'dotenv'
+dotenv.config()
 
-import connectDB from './db/connect.js';
+import connectDB from './db/connect.js'
 
 // routers
-import authRouter from './routes/authRoutes.js';
-import jobsRouter from './routes/jobsRoutes.js';
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js';
-import errorHandleMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandleMiddleware from './middleware/error-handler.js'
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Welcome!');
-});
+  res.send('Welcome!')
+})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandleMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandleMiddleware)
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 const start = async () => {
   try {
