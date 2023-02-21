bbf44a8d7cc4bc752cf3e9dfe233e00203b3e624
Shih-Yi <brvast@gmail.com>
2023-02-21 18:45:47 +1300

    1. install http-status-codes package 2. use status-codes 3. setup defaultError

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
 import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
 
 const register = async (req, res, next) => {
   try {
     console.log(req.body)
     const user = await User.create(req.body)
-    res.status(201).json({ user })
+    res.status(StatusCodes.CREATED).json({ user })
   } catch (error) {
     next(error)
   }
