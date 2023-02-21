7e5c81513456197667cbe36130d630e78113484b
Shih-Yi <brvast@gmail.com>
2023-02-21 17:25:22 +1300

    remove validator

--- models/User.js
+++ models/User.js
@@ -11,10 +11,6 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide a valid email',
-    },
     unique: true,
   },
   password: {
