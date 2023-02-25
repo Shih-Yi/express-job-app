faa498e733dd7cc838a5382290cf8e9142d0db4d
Shih-Yi <brvast@gmail.com>
2023-02-25 14:39:30 +1300

    add logout action

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
   login,
   updateUser,
   getCurrentUser,
+  logout,
 } from '../controllers/authController.js'
 import authenticateUser from '../middleware/auth.js'
 import rateLimiter from 'express-rate-limit'
@@ -20,5 +21,6 @@ router.route('/register').post(apiLimiter, register)
 router.route('/login').post(apiLimiter, login)
 router.route('/updateUser').patch(authenticateUser, updateUser)
 router.route('/getCurrentUser').get(authenticateUser, getCurrentUser)
+router.route('/logout').get(logout)
 
 export default router
