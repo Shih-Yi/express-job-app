f5efacfb2f8bfbb61d1aa37365d842a81b391008
Shih-Yi <brvast@gmail.com>
2023-02-28 12:21:00 +1300

    add production setup

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
 // Security Packages
 import helmet from 'helmet'
 import xss from 'xss-clean'
@@ -27,18 +31,24 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
 app.use(express.json())
 app.use(helmet())
 app.use(xss())
 app.use(mongoSanitize())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.send('Welcome!')
-})
-
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// only when ready to deploy
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandleMiddleware)
